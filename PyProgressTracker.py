de48ab5648d22dd6a9a8c14304e3c61f474a1b9d
dilruk25 <dilrukjayasinghe25@outlook.com>
2023-12-01 23:44:52 +0530

    Add 2 sections for Students and Staff. Add Progression list for Staff Member mode.

--- PyProgressTracker.py
+++ PyProgressTracker.py
@@ -1,10 +1,7 @@
 from graphics import *
-from myGraph import *
+import sys
 
-print("""***STUDENT PROGRESSION SYSTEM*** Developed by Dilruk""")
-print()
-
-# To ask selection of choice. (Continue or move to Histogram.)
+# Variable to ask selection of choice. (Continue or move to Histogram.)
 selection1 = 0
 
 progress_count = 0
@@ -12,8 +9,6 @@ trailer_count = 0
 retriever_count = 0
 excluded_count = 0
 global progress_outcome_list
-progress_outcome_list = []
-
 
 # ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 # validate inputs
@@ -21,10 +16,10 @@ def validation():
     global pass_credits
     global defer_credits
     global fail_credits
-
-    # -------------------------------------
-    # Prompt if it's not an integer.
+    
     while True:
+        # -------------------------------------
+        # Prompt if it's not an integer.
         try:
             pass_credits = int(input("Enter your total PASS credits: "))
 
@@ -71,17 +66,18 @@ def validation():
 
         # Total incorrect indication
         total_credits = pass_credits + defer_credits + fail_credits
-        if total_credits > 120:
+        if total_credits != 120:
             print("Total incorrect")
             pass
         else:
             return pass_credits, defer_credits, fail_credits
 
-
 # ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-def prompt_credits(pass_cred, defer_cred, fail_cred):
+# Prompt the progression outcome
+def prompt_credits(pass_credits, defer_credits, fail_credits):
     global credit_result
-    if pass_cred == 120:
+
+    if pass_credits == 120:
         credit_result = "Progress"
         global progress_count
         progress_count += 1
@@ -89,7 +85,7 @@ def prompt_credits(pass_cred, defer_cred, fail_cred):
         print("---------------------------------")
 
 
-    elif (pass_cred + defer_cred) > 100:
+    elif (pass_credits) >= 100:
         credit_result = "Progress (module trailer)"
         global trailer_count
         trailer_count += 1
@@ -97,36 +93,38 @@ def prompt_credits(pass_cred, defer_cred, fail_cred):
         print("---------------------------------")
         
 
-    elif (pass_cred + defer_cred) > 60:
-        credit_result = "Do not Progress – module retriever"
-        global retriever_count
-        retriever_count += 1
-        print(credit_result)
-        print("---------------------------------")
-        
-
-    else:
+    elif (fail_credits) >= 80:
         credit_result = "Exclude"
         global excluded_count
         excluded_count += 1
         print(credit_result)
         print("---------------------------------")
-    
+
+    else:
+        credit_result = "Do not Progress – module retriever"
+        global retriever_count
+        retriever_count += 1
+        print(credit_result)
+        print("---------------------------------")
+
     return progress_count, trailer_count, retriever_count, excluded_count, credit_result
 
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Store the progressions
+progress_outcomes_list = []
 def store_progress():
     global progress_list   
-    progress_list = [credit_result, pass_credits, defer_credits, fail_credits]
+    temp_progress_list = [credit_result, pass_credits, defer_credits, fail_credits]
+    
+    progress_outcomes_list.append(temp_progress_list)
 
-    progress_outcome_list.append(progress_list)
-
+    # Only use in histogram
     credit_list = [progress_count, trailer_count, retriever_count, excluded_count]
-    return progress_count, trailer_count, retriever_count, excluded_count, progress_outcome_list
+    return progress_count, trailer_count, retriever_count, excluded_count, progress_outcomes_list
 
 # ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-def histogram_graph(
-    progress_count, trailer_count, retriever_count, excluded_count, height_scale=25
-):
+# Display Histogram
+def histogram_graph(progress_count, trailer_count, retriever_count, excluded_count, height_scale = 25):
     # Create a GraphWin window
     win = GraphWin("Histogram", 715, 550)
 
@@ -146,100 +144,85 @@ def histogram_graph(
     excluded_percentage = (excluded_count / total_credits) * 100
 
     # Rectangles for each credits
-    progress_rect = Rectangle(
-        Point(70, 440), Point(190, 440 - progress_count * height_scale)
-    )
+    progress_rect = Rectangle(Point(100, 440), Point(220, 440 - progress_count * height_scale))
     progress_rect.setFill("green")
     progress_rect.draw(win)
 
-    trailer_rect = Rectangle(
-        Point(200, 440), Point(320, 440 - trailer_count * height_scale)
-    )
+    trailer_rect = Rectangle(Point(230, 440), Point(350, 440 - trailer_count * height_scale))
     trailer_rect.setFill("light green")
     trailer_rect.draw(win)
 
-    retriever_rect = Rectangle(
-        Point(330, 440), Point(450, 440 - retriever_count * height_scale)
-    )
+    retriever_rect = Rectangle(Point(360, 440), Point(480, 440 - retriever_count * height_scale))
     retriever_rect.setFill("orange")
     retriever_rect.draw(win)
 
-    excluded_rect = Rectangle(
-        Point(460, 440), Point(580, 440 - excluded_count * height_scale)
-    )
+    excluded_rect = Rectangle(Point(490, 440), Point(610, 440 - excluded_count * height_scale))
     excluded_rect.setFill("red")
     excluded_rect.draw(win)
 
     # Labels at the bottom of each bar
-    progress_label = Text(Point(130, 460), "Progress")
+    progress_label = Text(Point(160, 460), "Progress")
     progress_label.setSize(15)
     progress_label.setStyle("normal")
     progress_label.draw(win)
 
-    trailer_label = Text(Point(260, 460), "Trailer")
+    trailer_label = Text(Point(290, 460), "Trailer")
     trailer_label.setSize(15)
     trailer_label.setStyle("normal")
     trailer_label.draw(win)
 
-    retriever_label = Text(Point(390, 460), "Retriever")
+    retriever_label = Text(Point(420, 460), "Retriever")
     retriever_label.setSize(15)
     retriever_label.setStyle("normal")
     retriever_label.draw(win)
 
-    excluded_label = Text(Point(520, 460), "Excluded")
+    excluded_label = Text(Point(550, 460), "Excluded")
     excluded_label.setSize(15)
     excluded_label.setStyle("normal")
     excluded_label.draw(win)
 
     # Value labels on the top of each bar
-    progress_count_label = Text(
-        Point(130, 440 - progress_count * height_scale - 10), f"{progress_count}"
-    )
+    progress_count_label = Text(Point(160, 440 - progress_count * height_scale - 15), f"{progress_count}")
     progress_count_label.setSize(15)
     progress_count_label.setStyle("normal")
     progress_count_label.draw(win)
 
-    trailer_count_label = Text(
-        Point(260, 440 - trailer_count * height_scale - 10), f"{trailer_count}"
-    )
+    trailer_count_label = Text(Point(290, 440 - trailer_count * height_scale - 15), f"{trailer_count}")
     trailer_count_label.setSize(15)
     trailer_count_label.setStyle("normal")
     trailer_count_label.draw(win)
 
-    retriever_count_label = Text(
-        Point(390, 440 - retriever_count * height_scale - 10), f"{retriever_count}"
-    )
+    retriever_count_label = Text(Point(420, 440 - retriever_count * height_scale - 15), f"{retriever_count}")
     retriever_count_label.setSize(15)
     retriever_count_label.setStyle("normal")
     retriever_count_label.draw(win)
 
-    excluded_count_label = Text(
-        Point(520, 440 - excluded_count * height_scale - 10), f"{excluded_count}"
-    )
+    excluded_count_label = Text(Point(550, 440 - excluded_count * height_scale - 15), f"{excluded_count}")
     excluded_count_label.setSize(15)
     excluded_count_label.setStyle("normal")
     excluded_count_label.draw(win)
 
     # bottom x axis
-    start_point = Point(50, 440)
-    end_point = Point(600, 440)
+    start_point = Point(70,441)
+    end_point = Point(640, 441)
     line = Line(start_point, end_point)
     line.setOutline("black")
     line.setWidth(2)
     line.draw(win)
+    
 
     # Outcomes of the histogram
     title = Text(Point(190, 510), "{} outcomes in total".format(total_credits))
     title.setSize(20)
     title.setStyle("bold")
     title.draw(win)
-
+    
     # Display the Histogram window
     win.getMouse()
     win.close()
 
-
 # ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Selection Choice
 def selection_choice():
     global selection1
     while True:
@@ -256,24 +239,75 @@ Enter 'y' for yes or 'q' to quit and view results: """
         elif selection1 == "q":
             progression_report()
             histogram_graph(progress_count, trailer_count, retriever_count, excluded_count)
-            continue
+            break
 
         else:
             print("Incorrect selection")
             continue
 
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Progression Report
 def progression_report():
-    print(progress_outcome_list)
-    print(credit_result, "-", pass_credits, defer_credits, fail_credits)
+    print()
+    print("Part 2:")
+    for outcome in progress_outcomes_list:
+        print("---------------------------------")
+        print(f"{outcome[0]} - {outcome[1]}, {outcome[2]}, {outcome[3]}")
+    print("---------------------------------")
 
 # ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 # Start code
 # Part 1
-
+print("***STUDENT PROGRESSION SYSTEM*** Developed by Dilruk")
+print()
+print("Select the mode you want to continue")
+print("1. Student Mode")
+print("2. Staff Member Mode")
+print("---------------------------------")
+verify = input("Type 1 or 2: ").lower()
 while True:
-    print("Part 1:")
-    validation()
-    prompt_credits(pass_credits, defer_credits, fail_credits)
-    store_progress()
-    selection_choice()
-    
+    # For 1 Student
+    if verify == "1":
+        print("---------------------------------")
+        print("Student Mode") 
+        while True:  
+            validation()
+            prompt_credits(pass_credits, defer_credits, fail_credits)
+            store_progress()
+
+            selection2 = input("Do you want to Exit (y) or Try Again (n)? (y/n) ").lower()
+
+            if selection2 == "y":
+                print("In Student Mode, No histogram available for multiple progression.")
+                print("Have a Nice Day...")
+                sys.exit(0)
+            elif selection2 == "n":
+                continue
+            else:
+                print("Incorrect selection")
+                continue
+
+    # For a Staff Member
+    elif verify == "2":
+        print("---------------------------------")
+        print("Staff Member Mode")  
+        print("Part 1:")
+
+        while True:
+            validation()
+            prompt_credits(pass_credits, defer_credits, fail_credits)
+            store_progress()
+            selection_choice()
+            selection2 = input("Do you want to Exit (y) or Try Again (n)? (y/n) ").lower()
+            if selection2 == "y":
+                print("Have a Nice Day...")
+                sys.exit(0)
+            elif selection2 == "n":
+                continue
+            else:
+                print("Incorrect selection")
+                continue
+
+    else:
+        print("Incorrect selection. Try again")
+        continue
